4fbc9299397eff0c93124fbfc29ed0582025e52c
WinCS <94188592+Meltartica@users.noreply.github.com>
2024-12-24 23:23:56 +0800

    Adding Music

--- fireworks/script.js
+++ fireworks/script.js
@@ -1,141 +1,163 @@
-window.addEventListener("resize", resizeCanvas, false);
-window.addEventListener("DOMContentLoaded", onLoad, false);
+window.addEventListener('resize', resizeCanvas, false);
+window.addEventListener('DOMContentLoaded', onLoad, false);
 
-window.requestAnimationFrame = 
-    window.requestAnimationFrame       || 
-    window.webkitRequestAnimationFrame || 
-    window.mozRequestAnimationFrame    || 
-    window.oRequestAnimationFrame      || 
-    window.msRequestAnimationFrame     || 
-    function (callback) {
-        window.setTimeout(callback, 1000/60);
-    };
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
 
-var canvas, ctx, w, h, particles = [], probability = 0.04,
-    xPoint, yPoint;
+var canvas,
+  ctx,
+  w,
+  h,
+  particles = [],
+  probability = 0.04,
+  xPoint,
+  yPoint;
 
 function onLoad() {
-    var targetDateA = new Date(2024, 11, 25);
-    if (countdownA(targetDateA)) {
-        var container = document.createElement('div');
-        container.style.position = 'absolute';
-        container.style.top = '0';
-        container.style.left = '0';
-        container.style.width = '100%';
-        container.style.height = '100%';
-        container.style.pointerEvents = 'none';
-        document.body.appendChild(container);
+  var targetDateA = new Date(2024, 11, 25);
+  if (countdownA(targetDateA)) {
+    var container = document.createElement('div');
+    container.style.position = 'absolute';
+    container.style.top = '0';
+    container.style.left = '0';
+    container.style.width = '100%';
+    container.style.height = '100%';
+    container.style.pointerEvents = 'none';
+    document.body.appendChild(container);
 
-        canvas = document.createElement('canvas');
-        canvas.style.backgroundColor = 'transparent';
-        container.appendChild(canvas);
-        ctx = canvas.getContext("2d");
-        resizeCanvas();
-        
-        window.requestAnimationFrame(updateWorld);
-    }
-} 
+    canvas = document.createElement('canvas');
+    canvas.style.backgroundColor = 'transparent';
+    container.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+    resizeCanvas();
+
+    canplaythrough = true;
+
+    var audio = new Audio('./assets/that-christmas-night.mp3');
+    audio.addEventListener('canplaythrough', function() {
+      audio.play();
+    });
+
+    window.requestAnimationFrame(updateWorld);
+  }
+}
 
 function countdownA(targetDateA) {
-    const now = new Date();
-    let remainingSecondsA = Math.floor((targetDateA - now) / 1000);
+  const now = new Date();
+  let remainingSecondsA = Math.floor((targetDateA - now) / 1000);
 
-    if (remainingSecondsA <= 0) {
-        return true;
-    }
-    return false;
+  if (remainingSecondsA <= 0) {
+    return true;
+  }
+  return false;
 }
 
 function resizeCanvas() {
-    if (!!canvas) {
-        w = canvas.width = window.innerWidth;
-        h = canvas.height = window.innerHeight;
-    }
-} 
+  if (!!canvas) {
+    w = canvas.width = window.innerWidth;
+    h = canvas.height = window.innerHeight;
+  }
+}
 
 function updateWorld() {
-    update();
-    paint();
-    window.requestAnimationFrame(updateWorld);
-} 
+  update();
+  paint();
+  window.requestAnimationFrame(updateWorld);
+}
 
 function update() {
-    if (particles.length < 500 && Math.random() < probability) {
-        createFirework();
+  if (particles.length < 500 && Math.random() < probability) {
+    createFirework();
+  }
+  var alive = [];
+  for (var i = 0; i < particles.length; i++) {
+    if (particles[i].move()) {
+      alive.push(particles[i]);
     }
-    var alive = [];
-    for (var i=0; i<particles.length; i++) {
-        if (particles[i].move()) {
-            alive.push(particles[i]);
-        }
-    }
-    particles = alive;
-} 
+  }
+  particles = alive;
+}
 
 function paint() {
-    ctx.clearRect(0, 0, w, h);
-    ctx.globalCompositeOperation = 'lighter';
-    for (var i=0; i<particles.length; i++) {
-        particles[i].draw(ctx);
-    }
-} 
+  ctx.clearRect(0, 0, w, h);
+  ctx.globalCompositeOperation = 'lighter';
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].draw(ctx);
+  }
+}
 
 function createFirework() {
-    xPoint = Math.random()*(w-200)+100;
-    yPoint = Math.random()*(h-200)+100;
-    var nFire = Math.random()*50+100;
-    var c = "rgb("+(~~(Math.random()*200+55))+","
-         +(~~(Math.random()*200+55))+","+(~~(Math.random()*200+55))+")";
-    for (var i=0; i<nFire; i++) {
-        var particle = new Particle();
-        particle.color = c;
-        var vy = Math.sqrt(25-particle.vx*particle.vx);
-        if (Math.abs(particle.vy) > vy) {
-            particle.vy = particle.vy>0 ? vy: -vy;
-        }
-        particles.push(particle);
+  xPoint = Math.random() * (w - 200) + 100;
+  yPoint = Math.random() * (h - 200) + 100;
+  var nFire = Math.random() * 50 + 100;
+  var c =
+    'rgb(' +
+    ~~(Math.random() * 200 + 55) +
+    ',' +
+    ~~(Math.random() * 200 + 55) +
+    ',' +
+    ~~(Math.random() * 200 + 55) +
+    ')';
+  for (var i = 0; i < nFire; i++) {
+    var particle = new Particle();
+    particle.color = c;
+    var vy = Math.sqrt(25 - particle.vx * particle.vx);
+    if (Math.abs(particle.vy) > vy) {
+      particle.vy = particle.vy > 0 ? vy : -vy;
     }
-} 
+    particles.push(particle);
+  }
+}
 
 function Particle() {
-    this.w = this.h = Math.random()*4+1;
-    
-    this.x = xPoint-this.w/2;
-    this.y = yPoint-this.h/2;
-    
-    this.vx = (Math.random()-0.5)*10;
-    this.vy = (Math.random()-0.5)*10;
-    
-    this.alpha = Math.random()*.5+.5;
-    
-    this.color;
-} 
+  this.w = this.h = Math.random() * 4 + 1;
+
+  this.x = xPoint - this.w / 2;
+  this.y = yPoint - this.h / 2;
+
+  this.vx = (Math.random() - 0.5) * 10;
+  this.vy = (Math.random() - 0.5) * 10;
+
+  this.alpha = Math.random() * 0.5 + 0.5;
+
+  this.color;
+}
 
 Particle.prototype = {
-    gravity: 0.05,
-    move: function () {
-        this.x += this.vx;
-        this.vy += this.gravity;
-        this.y += this.vy;
-        this.alpha -= 0.01;
-        if (this.x <= -this.w || this.x >= screen.width ||
-            this.y >= screen.height ||
-            this.alpha <= 0) {
-                return false;
-        }
-        return true;
-    },
-    draw: function (c) {
-        c.save();
-        c.beginPath();
-        
-        c.translate(this.x+this.w/2, this.y+this.h/2);
-        c.arc(0, 0, this.w, 0, Math.PI*2);
-        c.fillStyle = this.color;
-        c.globalAlpha = this.alpha;
-        
-        c.closePath();
-        c.fill();
-        c.restore();
+  gravity: 0.05,
+  move: function () {
+    this.x += this.vx;
+    this.vy += this.gravity;
+    this.y += this.vy;
+    this.alpha -= 0.01;
+    if (
+      this.x <= -this.w ||
+      this.x >= screen.width ||
+      this.y >= screen.height ||
+      this.alpha <= 0
+    ) {
+      return false;
     }
-}
+    return true;
+  },
+  draw: function (c) {
+    c.save();
+    c.beginPath();
+
+    c.translate(this.x + this.w / 2, this.y + this.h / 2);
+    c.arc(0, 0, this.w, 0, Math.PI * 2);
+    c.fillStyle = this.color;
+    c.globalAlpha = this.alpha;
+
+    c.closePath();
+    c.fill();
+    c.restore();
+  },
+};
